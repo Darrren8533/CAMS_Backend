cde454f6b0a93b4ea9430bea7c7717358abbaf0e
DarrenLau <102768533@students.swinburne.edu.my>
2025-03-20 17:00:56 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -318,7 +318,17 @@ app.get('/users/customers', async (req, res) => {
   try {
     client = await pool.connect();
     const result = await client.query(`
-      SELECT userid, ufirstname, ulastname, uemail, uphoneno, ucountry, uzipcode, uactivation, ugender, utitle
+      SELECT 
+        userid AS "userID", 
+        ufirstname AS "uFirstName", 
+        ulastname AS "uLastName",
+        uemail AS "uEmail",
+        uphoneno AS "uPhoneNo",
+        ucountry AS "uCountry",
+        uzipcode AS "uZipcode",
+        uactivation AS "uActivation",
+        ugender AS "uGender",
+        utitle AS "uTitle"
       FROM users
       WHERE usergroup = 'Customer'
     `);
