6aeb91086050a474e2cfba0f110a3073e9922a51
DarrenLau <102768533@students.swinburne.edu.my>
2025-03-20 15:30:32 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -933,7 +933,7 @@ app.get('/checkStatus', async(req, res) => {
     
     // 修改查询，获取更多用户信息
     const query = {
-      text: 'SELECT userid, username, ustatus, uemail, ufirstname, ulastname FROM "users" WHERE "userid" = $1',
+      text: 'SELECT userid, username, ustatus, uemail, ufirstname, ulastname FROM "users" ',
       values: [userID]
     };
     
