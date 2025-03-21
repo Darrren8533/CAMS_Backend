b0bcf3b5bc17db8252a6525cf0c1adcb15b874c4
Oscar7453 <olwy0131@gmail.com>
2025-03-21 13:24:15 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -191,9 +191,9 @@ app.post('/login', async (req, res) => {
       res.status(200).json({
         message: 'Login Successful',
         success: true,
-        userID: "userID", 
-        userGroup: "userGroup",
-        uActivation: "uActivation" 
+        userID: userID, 
+        userGroup: userGroup,
+        uActivation: uActivation 
       });
     } else {
       res.status(401).json({ message: 'Invalid username or password', success: false });
