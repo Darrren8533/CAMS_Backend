3c202a7e731569259290452b47fffa1831e574f1
ChongKaiBo <128211069+ChongKaiBo@users.noreply.github.com>
2025-04-05 11:27:02 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1187,6 +1187,9 @@ app.post('/requestBooking/:reservationid', async (req, res) => {
       },
     });
 
+    console.log('EMAIL_USER:', process.env.EMAIL_USER);
+    console.log('EMAIL_PASS:', process.env.EMAIL_PASS);
+
     const mailOptions = {
       from: process.env.EMAIL_USER,
       to: userEmail,
